42abf0f1b337e61e236c6e2b0a9c160cb7a42e09
farah2003 <farah.shaqoura@example.com>
2022-05-26 02:16:29 +0300

    update note

--- keep/src/components/CardList/index.js
+++ keep/src/components/CardList/index.js
@@ -1,4 +1,6 @@
 import React,{useState} from 'react';
+import {db} from '../../firebase-config'
+import {updateDoc,doc} from 'firebase/firestore'
 import PropTypes from 'prop-types';
 import NoteCard from '../NoteCard/index';
 import NotesInput  from '../NotesInput/index';
@@ -6,16 +8,36 @@ import EditNotes from '../EditNotes';
 import './style.css'
 function CardList({view,notes,setIsUpdate}) {
     const [editModle,setEditModle]=useState(false)
+    const [updateNote,setUpdateNote]=useState({})
+
+    const displayCardContent=(item)=>{
+        setEditModle(true)
+        setUpdateNote(item)
+    }
+    const handleEditNote=async()=>{
+        try{
+            const userDocs=doc(db,"Notes",updateNote.id)
+            await updateDoc(userDocs,updateNote)
+            setIsUpdate(true)
+            setEditModle(false)
+          }catch(e){
+            console.log(e)
+          }
+    }
     return (
         <div className='card-list'>
             <div className='input-section'>
             <NotesInput setIsUpdate={setIsUpdate}/>  
             </div>
             {editModle?
-            <EditNotes setEditModle={setEditModle}/>
+            <EditNotes 
+            handleEditNote={handleEditNote} 
+            updateNote={updateNote}
+            setUpdateNote={setUpdateNote}/>
            :  <div className={`${view}`}>
                {notes.map((item)=>{
-                return (<div key={item.id} onClick={()=>setEditModle(true)}>
+                return (
+                <div key={item.id} onClick={()=>displayCardContent(item)}>
                  <NoteCard view={view} item={item}/>
                  </div>)
                })}
--- keep/src/components/EditNotes/index.js
+++ keep/src/components/EditNotes/index.js
@@ -4,11 +4,18 @@ import {BsBell} from 'react-icons/bs'
 import {BiUserPlus,BiImage,BiArchiveIn}from 'react-icons/bi'
 import {FiMoreVertical}from 'react-icons/fi'
 import './style.css'
-function EditNotes({setEditModle}) {
+function EditNotes({handleEditNote,updateNote,setUpdateNote}) {
+
     return (
         <div className='edit-modle'>
-        <input  className="edit-modle-title" value={'fgjhkhfghfghkjfghkjgf'} />
-        <input    className='edit-modle-content' value={'fgjhkhfghfghkjfghkjgf'}/>
+        <input 
+         className="edit-modle-title"
+         value={updateNote.title} 
+         onChange={(e)=>setUpdateNote({...updateNote,title:e.target.value})} />
+        <input 
+        className='edit-modle-content'
+         value={updateNote.content}
+         onChange={(e)=>setUpdateNote({...updateNote,content:e.target.value})}/>
         <p>Edited 2:19 AM</p>
         <div className='edit-notes-footer'>
      <div className='edit-notes-icons'>
@@ -28,14 +35,16 @@ function EditNotes({setEditModle}) {
          <FiMoreVertical/>
      </div>
      </div>
-     <span onClick={()=>setEditModle(false)}>Close</span>
+     <span onClick={()=>handleEditNote()}>Close</span>
         </div>
     </div>
     );
 }
 
 EditNotes.propTypes = {
-    setEditModle:PropTypes.func.isRequired,
+    handleEditNote:PropTypes.func.isRequired,
+    updateNote:PropTypes.objectOf.isRequired,
+    setUpdateNote:PropTypes.func.isRequired
      };
    
 export default EditNotes;
